b606551acf71f356b3c3a7582981e100165bc010
Timothy7310 <penlist127@gmail.com>
2022-05-06 13:43:57 +0300

    feat: add render keyboard

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,9 @@
 import '../scss/main.scss';
 import '../index.html';
+import keyBoradSetUp from './modules/keyboard-setup';
+import KeyBoard from './modules/keyboard';
+
+const keyboard = new KeyBoard(keyBoradSetUp, 'body', navigator.userAgent);
+keyboard.render();
+keyboard.languageMessage();
+keyboard.operatingSystemMessage();
---
+++ src/js/modules/keyboard.js
@@ -0,0 +1,42 @@
+class KeyBoard {
+    constructor(setUp, parentSelector, system) {
+        this.setUp = setUp;
+        this.parent = document.querySelector(parentSelector);
+        this.system = system;
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.classList.add('keyboard');
+        element.innerHTML = this.setUp;
+        this.parent.append(element);
+    }
+
+    languageMessage() {
+        const message = document.createElement('p');
+        message.classList.add('language');
+        message.innerHTML = `Для смены языка используйте:
+                            <span class="accent-text">Ctrl + Alt</span>
+                            `;
+        this.parent.append(message);
+    }
+
+    operatingSystemMessage() {
+        const message = document.createElement('p');
+        let systemType;
+        message.classList.add('operating');
+        if (this.system.indexOf('Win') !== -1) {
+            systemType = 'Windows';
+        } else if (this.system.indexOf('Mac') !== -1) {
+            systemType = 'Macintosh';
+        } else if (this.system.indexOf('Linux') !== -1) {
+            systemType = 'Linux';
+        }
+        message.innerHTML = `Клавиатура создана в операционной системе
+                            <span class="accent-text">${systemType}</span>
+                            `;
+        this.parent.append(message);
+    }
+}
+
+export default KeyBoard;
