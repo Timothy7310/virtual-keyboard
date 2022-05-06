884ebe66df5884a2fcc5c3ed34648694dc870b6b
Timothy7310 <penlist127@gmail.com>
2022-05-06 15:46:36 +0300

    feat: add change language

--- src/js/modules/keyboard-listener.js
+++ src/js/modules/keyboard-listener.js
@@ -1,7 +1,16 @@
+import changeLang from './change-language';
+
+let isEng = false;
+
 const keyboardListener = () => {
     document.addEventListener('keydown', (e) => {
         e.preventDefault();
         document.querySelector(`.${e.code}`).classList.add('active');
+
+        if ((e.ctrlKey || e.metaKey) && e.altKey) {
+            changeLang();
+            isEng = !isEng;
+        }
     });
 
     document.addEventListener('keyup', (e) => {
